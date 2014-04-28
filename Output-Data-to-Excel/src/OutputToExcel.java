3bf2600f6e0b0f6ad1f4e0e4ffacbd51024d28bf
Yu-Ting <yutingkao23@gmail.com>
2014-04-28 22:14:11 +0800

    fix arguments check.

--- Output-Data-to-Excel/src/OutputToExcel.java
+++ Output-Data-to-Excel/src/OutputToExcel.java
@@ -4,9 +4,13 @@ import java.io.IOException;
 public class OutputToExcel {
 
 	public static void main(String[] args) {
+		if (args.length != 2) {
+			System.err.println("arguments is not 2.");
+		}
+		
 		InputDatas input = null;
 		try {
-			input = new InputDatas(args[1]);
+			input = new InputDatas(args[0]);
 		} catch (FileNotFoundException e1) {
 			e1.printStackTrace();
 		}		
@@ -18,7 +22,7 @@ public class OutputToExcel {
 			e1.printStackTrace();
 		}
 		
-		ExcelDoc doc1 = new ExcelDoc(args[2]);
+		ExcelDoc doc1 = new ExcelDoc(args[1]);
 		doc1.createWorkbook();
 		doc1.createSheet();
 		
